4d2289008baf1055917b19f11ed13f7d9128d3c2
Akash <akasharade8@gmail.com>
2025-06-10 00:44:07 +0530

    first commit

---
+++ src/main/java/com/akash/ApiGateway/filter/JwtValidationFilter.java
@@ -0,0 +1,49 @@
+package com.akash.ApiGateway.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationFilter extends AbstractGatewayFilterFactory<JwtValidationFilter.Config> {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    public JwtValidationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String token = authHeader.replace("Bearer ", "");
+
+            return webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/api/auth/validate?token=" + token) // use direct localhost if no service registry
+                    .retrieve()
+                    .toBodilessEntity()
+                    .flatMap(response -> chain.filter(exchange))
+                    .onErrorResume(error -> {
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    });
+        };
+    }
+
+    public static class Config {
+        //Further Modification if required for this application
+    }
+}
